d0780e6993f5652fe0b5507eb53f0d2029384429
Ryan Ong <ryanong@gmail.com>
2014-01-07 15:21:00 -0500

    add #phone_number

--- test/person_test.rb
+++ test/person_test.rb
@@ -1,28 +1,32 @@
 require "test_helper"
 
 class PersonTest < Minitest::Test
-  def setup
-    @data = {
-      :first_name => "ryan",
-      last_name: "ong"
-    }
-  	@person = Fictional::Person.new(@data)
-    
-    @data2 = {
-      first_name: "sam",
-      last_name: "asshole"
-    }
-    @person2 = Fictional::Person.new(@data2)
-  end
 
   def test_first_name
     assert name1 = Fictional::Person.first_name
     assert name2 = Fictional::Person.first_name
     refute_equal name1, name2
   end
+  
   def test_last_name
     assert name1 = Fictional::Person.last_name
     assert name2 = Fictional::Person.last_name
     refute_equal name1, name2
+  end
+
+  def test_build
+    assert person1 = Fictional::Person.build
+    assert person2 = Fictional::Person.build
+    refute_equal person1.first_name, person2.first_name
+    refute_equal person1.last_name, person2.last_name
+  end
+  
+  def test_built_email
+    assert person1 = Fictional::Person.build
+    assert person1.email
+  end
+  
+  def test_phone_number
+    assert_match /\A\d{3}-\d{3}-\d{4}\Z/, Fictional::Person.phone_number
   end
 end
